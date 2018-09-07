5fb7680327d295a68fd9230bb2ed0cc84b170658
rmar72 <rsolm72@gmail.com>
2018-09-07 06:08:31 -0600

    user model strict mode requiring such field

--- models/User.js
+++ models/User.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 
-const { Schema }  = mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    username: String,
-    email:  String,
-    hash:   String,
-    salt:   String
+    username: { type: String, required: true },
+    email:  { type: String, required: true },
+    hash:   { type: String, required: true },
+    salt:   { type: String, required: true }
 });
 
 UserSchema.methods.setPassword = function(password){
