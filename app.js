19983c4e61e1881a2927a793ca1715b648877942
rmar72 <rsolm72@gmail.com>
2018-09-07 03:04:24 -0600

    app configurations

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const path = require("path") ;
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Db setup
+mongoose.promise = global.Promise;
+mongoose.connect("mongodb://localhost/youtweet");
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 's',
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: false
+}));
+
+
 
 
 app.get('/', (req, res) => res.send('The world is my oyster.'))
