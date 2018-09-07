f1185f028e5c2c5683717526eb9a576bc3eade70
rmar72 <rsolm72@gmail.com>
2018-09-07 06:38:58 -0600

    User Model JWT methods for token generation & for returning token

--- models/User.js
+++ models/User.js
@@ -21,4 +21,24 @@ UserSchema.methods.validatePassword = function(password){
     return this.hash === hash;
 }
 
+UserSchema.methods.generateJWT = function(){
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        email: this.email,
+        id: this._id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10)
+    }, 'secret');
+}
+
+UserSchema.methods.toAuthJSON = function(){
+    return{
+        _id: this._id,
+        email: this.email,
+        token: this.generateJWT()
+    }
+}
+
 mongoose.model('User', UserSchema);
