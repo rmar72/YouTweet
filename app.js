97e5b9686486b370279878f913d506bedb97d192
rmar72 <rsolm72@gmail.com>
2018-09-06 23:23:46 -0600

    dependencies installment and setup

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path") ;
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const logger = require("morgan");
+
 
 app.get('/', (req, res) => res.send('The world is my oyster.'))
 
