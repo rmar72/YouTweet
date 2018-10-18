f408460f870a30c3e032f5e740eb6230201a95d6
rsol7 <rsolm72@gmail.com>
2018-10-18 01:02:38 -0600

    protected route for tweeting videos, at this point it's retrieving the video id (enough to compose a url to view that YT video)

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const router = require("express").Router();
 const User =  mongoose.model('User');
+const token_auth = require("../config/token_auth");
 
+router.post('/tweet-video', token_auth.required, (req, res) => {
+    let url ='https://www.youtube.com/watch?v='+ req.body.video.url;
+    console.log(url)
+    res.json({'video to be tweeted': url})
+});
 
 module.exports = router;
